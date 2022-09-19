73356bc340fce8d1fd23335e52ebf16f2e223fd3
Marcos <marcosprogramador123@gmail.com>
2022-09-19 13:09:28 -0300

    tabu

---
+++ tabuada/tabuada.py
@@ -0,0 +1,23 @@
+numero = int(input('escreva o número que queira a tabuada:'))
+conta1 = numero * 1
+conta2 = numero * 2
+conta3 = numero * 3
+conta4 = numero * 4
+conta5 = numero * 5
+conta6 = numero * 6
+conta7 = numero * 7
+conta8 = numero * 8
+conta9 = numero * 9
+conta10 = numero * 10
+
+print('{} * 1 = {}'.format(numero, conta1))
+print('{} * 2 = {}'.format(numero, conta2))
+print('{} * 3 = {}'.format(numero, conta3))
+print('{} * 4 = {}'.format(numero, conta4))
+print('{} * 5 = {}'.format(numero, conta5))
+print('{} * 6 = {}'.format(numero, conta6))
+print('{} * 7 = {}'.format(numero, conta7))
+print('{} * 8 = {}'.format(numero, conta8))
+print('{} * 9 = {}'.format(numero, conta9))
+print('{} * 10 = {}'.format(numero, conta10))
+
