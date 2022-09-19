dcfb470a8e15b6b1fb29ff7f58439b19cf41a58c
Marcos <marcosprogramador123@gmail.com>
2022-09-19 12:04:48 -0300

    o algoritmo

---
+++ ALGORITMO/algoritmo.py
@@ -0,0 +1,6 @@
+numero = int(input('digite seu número:'))
+dobro = numero * 2;
+triplo = numero * 3;
+raiz = numero ** (1/2)
+
+print('o dobro de {} é {}, o triplo dele é {} e a raiz quadrada dele é {}'.format(numero, dobro, triplo, raiz))
