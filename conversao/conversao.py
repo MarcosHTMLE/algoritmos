2269dd3e9f6fa93c743992b2bf8c9ad720f9641d
Marcos <marcosprogramador123@gmail.com>
2022-09-19 13:05:44 -0300

    converti

---
+++ conversao/conversao.py
@@ -0,0 +1,5 @@
+metros = int((input('descreva a quantidade de metros')))
+centimetros = metros * 100
+milimetros = metros * 1000
+
+print('{} metros são {}cm, e em milimetros são {}ml'.format(metros, centimetros, milimetros))
