f485b37e05ba9f46f9ce7afd280470eaa92156c4
Marcos <marcosprogramador123@gmail.com>
2022-09-19 12:10:53 -0300

    o perimetro

---
+++ perimetro/area.py
@@ -0,0 +1,10 @@
+largura = float(input('qual o tamanho da largura em metros?'))
+altura = float(input('qual o tamanho da altura em metros?'))
+
+area = largura * altura
+
+quantidade = area / 2
+
+print('a área da parede é {}m e você precisa de {} litros de tinta para pintá-la'.format(area, quantidade))
+
+
